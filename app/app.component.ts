58c585f3341b3175f71ddbf0218b14e20a8cfd35
Michael Schennum <mikeschenphoto@gmail.com>
2016-03-21 14:15:50 -0700

    adds blue to done tasks

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,70 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 
 @Component({
-  selector: 'my-app',
+    selector: 'task-display',
+    inputs: ['task'],
   template: `
-  <div class="container">
-    <h1>ToDo List App!</h1>
-    <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
-    {{ task.description }} 
-    </h3>
-  </div>
+    <h3>{{ task.description }}</h3>
   `
 })
 
-export class AppComponent {
-	public tasks: Task[];
-	constructor() {
-		this.tasks = [
-			new Task("Create To-Do List app.", 0),
-			new Task("Learn Kung Fu.", 1),
-			new Task("Rewatch all the Lord of the Rings movies.", 2),
-			new Task("Do the laundry.", 3)
-		];
-	}
+export class TaskComponent {
+  public task: Task;
+}
+
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  directives: [TaskComponent],
+  template: `
+  <task-display *ngFor="#currentTask of taskList"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask"
+    [task]="currentTask">
+  </task-display>
+  `
+})
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
+
+
+@Component({
+  selector: 'my-app',
+  directives: [TaskListComponent],
+  template: `
+    <div class="container">
+      <h1>To-Do List</h1>
+      <task-list 
+        [taskList]="tasks"
+        (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
+    <div>
+  `
+})
+export class AppComponent { 
+  public tasks: Task[];
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
+  }
   taskWasSelected(clickedTask: Task): void {
-    console.log(clickedTask);
+    console.log('parent', clickedTask);
   }
 }
 
